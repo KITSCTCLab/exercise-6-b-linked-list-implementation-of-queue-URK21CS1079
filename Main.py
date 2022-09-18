13089f4149754c7a49c9b65ef24f7f4f0d4c61b8
URK21CS1079 <110010648+URK21CS1079@users.noreply.github.com>
2022-09-18 21:27:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,88 +1,49 @@
 class Node:
   def __init__(self, data):
-
     self.data = data
-
     self.next = None
 
 
 class Queue:
-
   def __init__(self):
-
     self.head = None
-
     self.last = None
 
-
   def enqueue(self, data) -> None:
-
       last = self.last
-
       node = Node(data)
-
       if(self.last is None and self.head is None):
-
         self.last , self.head = node , node
-
       else:
-
         self.last.next = node
-
         self.last = node
 
-
   def dequeue(self) -> None:
-
-    if self.head is not None:
-
-      self.head = self.head.next
-
+    if self.head == self.last:
+        self.head = self.last = None
     else:
+        self.head = self.head.next
 
-      print("Queue is Empty")
-
-
-    if self.head.next is None:
-
-      self.head = None
-
-      self.last = None
-
+    
 
   def status(self) -> None:
-
     ptr = self.head
-
-    while ptr:
-
+    while ptr != None:
       print(ptr.data,end = "=>")
-
       ptr = ptr.next
-
     print("None")
 
 
+# Do not change the following code
 queue = Queue()
-
 operations = []
-
 for specific_operation in input().split(','):
-
     operations.append(specific_operation.strip())
-
 input_data = input()
-
 data = input_data.split(',')
-
 for i in range(len(operations)):
-
   if operations[i] == "enqueue":
-
     queue.enqueue(int(data[i]))
-
   elif operations[i] == "dequeue":
-
     queue.dequeue()
-
 queue.status()
